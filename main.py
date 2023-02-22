3a581f2ddd94327ef6ed4f3d6d36f4a7358ac9b4
elanur1903 <120015112+elanurbahsi@users.noreply.github.com>
2023-02-22 12:20:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+def oyna(rakam):
+  sayi=random.randint(1,100)
+  print(sayi)
+  print(rakam)
+def cagir():
+    oyna(54)
+
+cagir()
+
+if oyna== cagir:
+  print('kazandın')
+
+else:
+  print('kazanamadın')
